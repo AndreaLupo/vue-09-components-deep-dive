98da665b6f3b10f3c4f993ccd48890f4ef187491
Wolf Developer <lupo.andrea.93@gmail.com>
2022-11-19 10:04:45 +0100

    Scoped slots

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,13 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+
+    <course-goals>
+      <template #default="myProps">
+        <h2>{{myProps.item}}</h2>
+        <p>{{myProps['another-prop']}}</p>
+      </template>
+    </course-goals>
   </div>
 </template>
 
@@ -14,6 +21,7 @@
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
   data() {
@@ -28,7 +36,8 @@ export default {
   components: {
     TheHeader,
     BadgeList,
-    UserInfo
+    UserInfo,
+    CourseGoals
   }
 };
 </script>
